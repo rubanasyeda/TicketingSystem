01dd7d1c8e1369ff81b9c61471aadf92c050d125
MohammadHafiz Kapadwala <wbj741@usask.ca>
2023-10-23 15:49:23 -0600

    Created a html file for the front end people to work on

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "Team_43_CMPT_370"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    create_database(app)
     return app
+
+    return app
+
+
+def create_database(app):
+    with app.app_context():
+        db.create_all()
+    print("Database Created!!")
