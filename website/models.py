109c2b21a5815fe7c34e851e898da444fefda377
MohammadHafiz Kapadwala <wbj741@usask.ca>
2023-10-24 22:15:44 -0600

    Created enum for the status of the ticket

--- website/models.py
+++ website/models.py
@@ -1,2 +1,12 @@
 #Will have all classes for database#
 
+from . import db
+from sqlalchemy.sql import func
+from sqlalchemy import Column , Enum
+from enum import Enum as EnumBase
+
+#priority for status of my tickets
+class statusEnum(EnumBase):
+    RESOLVED = "resolved"
+    UNRESOLVED = "unresolved"
+
