7ddabbea17bd67b3bb7a114f6228c17a9319e305
MohammadHafiz Kapadwala <wbj741@usask.ca>
2023-10-24 22:59:21 -0600

    Completed my createTicket route in view.py

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,17 @@
 #will have all the routes for authorization pages#
 from flask import Blueprint,render_template,request,redirect,url_for
-
 auth = Blueprint('auth',__name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/signup',methods=['GET', 'POST'])
+def signup():
+    return render_template('signup.html')
+
+
+@auth.route('/logout')
+def logout():
+    return redirect(url_for('views.home'))
--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
 #will have all the routes for pages that does not have authorization#
 
 from flask import Blueprint,render_template,request,redirect,url_for,jsonify
-from flask_login import login_required
 from datetime import datetime
 from . import db
+from .models import CusomterTickerInformation
 
 views = Blueprint('views', __name__)
 
@@ -12,6 +12,25 @@ def home():
     return render_template("landing.html")
 
 @views.route("/dashboard",methods=['GET'])
-@login_required
 def dashboard():
     return render_template("dashboard.html")
+
+@views.route('/createTicket',methods=['GET','POST'])
+def createTicket():
+    if request.method == "POST":
+        subject = request.form.get('subject')
+        customerFirstName = request.form.get('customer_first_name')
+        customerLastName = request.form.get('customer_last_name')
+        customerEmail = request.form.get('customer_email')
+        businessName = request.form.get('business_name')
+        customerNumber = request.form.get('customer_phone')
+        problemDescription = request.form.get('description')
+
+        customerInfo = CusomterTickerInformation(subject=subject,firstName=customerFirstName,lastName=customerLastName,
+                                                 email=customerEmail,businessName=businessName,phoneNumber=customerNumber,
+                                                 description=problemDescription)
+        db.session.add(customerInfo)
+        db.session.commit()
+        return redirect(url_for('views.home'))
+    return render_template("createTicket.html")
+
