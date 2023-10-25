73571d7d15edf59532007ad7e77034b3c8a0ebce
Marwan Mostafa <mam024@usask.com>
2023-10-25 00:26:31 -0500

    setup flask login manager for user sessions, and added routes for worker login, admin creation of worker acounts, and loging out

--- website/__init__.py
+++ website/__init__.py
@@ -1,33 +1,46 @@
-#This will make our wesbite folder as package#
-
+#This python file  will allows us to import the website folder as package
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-
 def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = "Team_43_CMPT_370"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+     app = Flask(__name__)
+     app.config['SECRET_KEY'] = "Team_43_CMPT_370"
+     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+     db.init_app(app)
 
-    from .views import views
-    from .auth import auth
+     login_manager = LoginManager()
+     login_manager.login_view = 'auth.login'
+     login_manager.init_app(app)
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+     from .models import User
 
-    from .models import CusomterTickerInformation
-    from .models import User
 
-    create_database(app)
-    return app
+     from .views import views
+     from .auth import auth
 
-    return app
+     app.register_blueprint(views, url_prefix='/')
+     app.register_blueprint(auth, url_prefix='/')
+
+
+     from .models import CusomterTickerInformation
+
+     @login_manager.user_loader
+     def load_user(user_id):
+          return User.query.get(int(user_id))
+
+     create_database(app)
+
+
+     return app
 
 
 def create_database(app):
-    with app.app_context():
-        db.create_all()
-    print("Database Created!!")
+     with app.app_context():
+          db.create_all()
+     print("Database Created!!")
--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,56 @@
-#will have all the routes for authorization pages#
 from flask import Blueprint,render_template,request,redirect,url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+from flask_login import login_user,login_required, current_user, logout_user
+
 auth = Blueprint('auth',__name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if not user or not check_password_hash(user.password, password):
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=True)
+        return redirect(url_for('views.dashboard'))
     return render_template("login.html")
 
+
 @auth.route('/signup',methods=['GET', 'POST'])
+@login_required  # remove if no inital acocunt is in data base
 def signup():
-    return render_template('signup.html')
+    
+    if current_user.is_authenticated == False:
+        return redirect(url_for('auth.login'))
+
+    user = User.query.filter_by(id=current_user.get_id()).first()
+    if user.role == "admin":
+
+        if request.method == 'POST':
+            name = request.form.get("name")
+            username = request.form.get("username")
+            password = request.form.get("password")
+            role = request.form.get("role")
+            user = User.query.filter_by(username=username).first()
+            if user:
+                return redirect(url_for('auth.signup'))
+            new_user = User(name=name, username=username, password=generate_password_hash(password, method='sha256'),
+                            role=role)
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('auth.login'))
+    
+        return render_template('signup.html')
 
 
 @auth.route('/logout')
+@login_required
 def logout():
+    logout_user()
     return redirect(url_for('views.home'))
