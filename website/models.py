54fab462c5bf38c6290263d7814cc041293d5044
MohammadHafiz Kapadwala <wbj741@usask.ca>
2023-10-24 22:28:16 -0600

    Completed with Customer Ticket Information Database creation

--- website/models.py
+++ website/models.py
@@ -16,3 +16,16 @@ class priorityOrder(EnumBase):
     HIGHPRIORITY = "highpriority"
     LOWPRIORITY = "lowpriority"
     NONE = "none"
+
+class CusomterTickerInformation(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    subject = db.Column(db.String(150))
+    firstName = db.Column(db.String(150))
+    lastName = db.Column(db.String(150))
+    email = db.Column(db.String(150))
+    businessName = db.Column(db.String(150))
+    phoneNumber = db.Column(db.String(20))
+    description = db.Column(db.String(500))
+    status = Column(Enum(statusEnum),default=statusEnum.UNRESOLVED)
+    priority = Column(Enum(priorityOrder),default=priorityOrder.NONE)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
